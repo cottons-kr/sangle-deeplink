b54b19166ef77227966ea93a6646b041bd0c54d2
cottons-kr <taeyeong.kim@algorix.io>
2024-11-24 04:09:24 +0900

    ✨ 생글 앱 런치 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
-import { VStack } from '@cottons-kr/react-foundation'
+'use client'
+
+import { IParams, VStack } from '@cottons-kr/react-foundation'
+import { use, useEffect } from 'react'
 
 import s from '@/styles/page.module.scss'
 
-export default function IndexPage() {
+const PROTOCOL = 'sangle-deeplink' as const
+
+export default function IndexPage(props: IParams<'', 'id'>) {
+  const searchParams = use(props.searchParams)
+  const { id } = searchParams
+
+  useEffect(() => {
+    if (!id) {
+      return alert('잘못된 접근입니다.')
+    }
+
+    window.location.href = `${PROTOCOL}://share?id=${id}`
+  }, [id])
+
   return <>
     <VStack align='center'>
       <img className={s.image} src='/assets/deeplink.png' alt='오른쪽 화살표' />
